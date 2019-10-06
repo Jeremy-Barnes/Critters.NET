03c7e50cec5565b0c742d0cdfc24ebe3b38e8614
Jeremy Barnes <Jeremiah.Barnes@outlook.com>
2019-10-05 23:25:16 -0500

    Stub

---
+++ CritterServer/Controllers/NpcController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CritterServer.Controllers
+{
+    [Route("api/npc")]
+    [ApiController]
+    public class NpcController : ControllerBase
+    {
+        public NpcController()
+        {
+
+        }
+    }
+}
