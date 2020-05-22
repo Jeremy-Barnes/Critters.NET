b7ec429637e2da59e090eb2da8c3fd0649beaae0
Jeremy Barnes <Jeremiah.Barnes@outlook.com>
2020-05-21 20:17:46 -0500

    Integration tests

---
+++ Tests/IntegrationTests/MessageTests.cs
@@ -0,0 +1,269 @@
+﻿using CritterServer.Models;
+using CritterServer.DataAccess;
+using CritterServer.Domains;
+using CritterServer.Domains.Components;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using Xunit;
+using CritterServer.Contract;
+using System.Linq;
+
+namespace Tests.IntegrationTests
+{
+    /// <summary>
+    /// Creaated once, reused for all tests in UserTests
+    /// Used to hold expensive resources that can be reused (like a DB connection!)
+    /// </summary>
+    public class MessageTestsContext
+    {
+        private static string jwtSecretKey = "T25lIEV4Y2VwdGlvbmFsbHkgTG9uZyBTZWNyZXQgS2V5IFBsZWFzZSEgRm9yIFJlYWwhIEV2ZW4gTG9uZ2VyIFRoYW4gWW91J2QgUmVhc29uYWJseSBBbnRpY2lwYXRl";
+
+        public IDbConnection dbConnection;
+        public UserDomain userAccountDomain;
+        public NotificationDomain messageDomain;
+        public IUserRepository userRepo;
+        public IMessageRepository messageRepo;
+
+        public User AUser1;
+        public User AUser2;
+
+        public User BUser1;
+        public User BUser2;
+
+        public JwtProvider jwtProvider = new JwtProvider(
+            jwtSecretKey,
+            new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(jwtSecretKey)),
+                ValidIssuer = "critters!",
+                ValidateAudience = false,
+                ValidateActor = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                RequireExpirationTime = true
+            });
+
+        public List<User> extantUsers = new List<User>();
+
+        public MessageTestsContext()
+        {
+            DbProviderFactories.RegisterFactory("Npgsql", Npgsql.NpgsqlFactory.Instance);
+            dbConnection = DbProviderFactories.GetFactory("Npgsql").CreateConnection();
+            dbConnection.ConnectionString = "Server=localhost; Port=5432; User Id=LocalApp;Password=localapplicationpassword;Database=CrittersDB";
+            userRepo = new UserRepository(dbConnection);
+            userAccountDomain = new UserDomain(userRepo, jwtProvider);
+            messageRepo = new MessageRepository(dbConnection);
+            messageDomain = new NotificationDomain(messageRepo, userAccountDomain, null);
+
+            AUser1 = RandomUser();
+            AUser2 = RandomUser();
+            BUser1 = RandomUser();
+            BUser2 = RandomUser();
+            userAccountDomain.CreateAccount(AUser1).Wait();
+            userAccountDomain.CreateAccount(AUser2).Wait();
+
+            userAccountDomain.CreateAccount(BUser1).Wait();
+            userAccountDomain.CreateAccount(BUser2).Wait();
+        }
+
+        public User RandomUser()
+        {
+            User randomUser = new User()
+            {
+                Birthdate = DateTime.UtcNow,
+                City = "Chicago",
+                Country = "USA",
+                EmailAddress = $"{TestUtils.GetRandomString(6)}@{TestUtils.GetRandomString(6)}.com",
+                FirstName = TestUtils.GetRandomString(6),
+                LastName = TestUtils.GetRandomString(6),
+                Gender = "male",
+                IsActive = true,
+                Password = TestUtils.GetRandomString(6),
+                Postcode = "60654",
+                Salt = "GARBAGEVALUE",
+                State = "Illinois",
+                UserName = TestUtils.GetRandomString(6)
+            };
+            this.extantUsers.Add(randomUser);
+            return randomUser;
+        }
+
+        public List<int> SendMessage(User sender, User receiver, int times, int channel)
+        {
+            List<int> messageIds = new List<int>();
+            for (int i = 0; i < times; i++) {
+                messageIds.Add(messageDomain.SendMessage(new Message
+                {
+                    MessageSubject = $"This message created at {DateTime.UtcNow}",
+                    MessageText = $"My dearest {receiver.FirstName}, I hope this component test passes and finds you well. Happy {DateTime.UtcNow.DayOfWeek}! -{sender.FirstName}",
+                    SenderUserName = sender.UserName,
+                    ChannelId = channel
+                },
+                    sender).Result);
+            }
+            return messageIds;
+        }
+    }
+
+    public class MessageTests: IClassFixture<MessageTestsContext>
+    {
+        MessageTestsContext context;
+
+        public MessageTests(MessageTestsContext context)
+        {
+            this.context = context;
+        }
+
+        [Fact]
+        public void CreateChannelAddsCreator()
+        {
+            var generatedChannelId = context.messageDomain.CreateChannel(context.AUser1, $"{TestUtils.GetRandomString(30)}Component", context.extantUsers.Select(s => s.UserName).ToList()).Result;
+            Assert.IsType<int>(generatedChannelId);
+            var channels = context.messageDomain.FindChannelWithUsers(null, context.AUser1).Result;
+            Assert.Contains(channels, c => c.ChannelId == generatedChannelId);
+        }
+
+        [Fact]
+        public void SendMessageGetsIntDbId()
+        {
+            var generatedChannelId = context.messageDomain.CreateChannel(context.AUser1, $"{TestUtils.GetRandomString(30)}Component", new List<string> { context.AUser2.UserName }).Result;
+            var messageIdGenerated = context.messageDomain.SendMessage(new Message { 
+                MessageSubject = $"This message created at {DateTime.UtcNow}", 
+                MessageText = $"My dearest {context.AUser2.FirstName}, I hope this component test passes and finds you well. Happy {DateTime.UtcNow.DayOfWeek}! -{context.AUser1.FirstName}",
+                SenderUserName = context.AUser1.UserName,
+                ChannelId = generatedChannelId
+            },
+                context.AUser1).Result;
+            Assert.IsType<int>(messageIdGenerated);
+        }
+
+
+        [Fact]
+        public void SendMessageToNonMemberChannelFails()
+        {
+            var generatedChannelId = context.messageDomain.CreateChannel(context.AUser2, $"{TestUtils.GetRandomString(30)}Component", null).Result;
+
+            Assert.Throws<AggregateException>(() => context.messageDomain.SendMessage(new Message
+            {
+                MessageSubject = $"This message created at {DateTime.UtcNow}",
+                MessageText = $"My dearest {context.AUser2.FirstName}, I hope this component test passes and finds you well. Happy {DateTime.UtcNow.DayOfWeek}! -{context.AUser1.FirstName}",
+                SenderUserName = context.AUser1.UserName
+            },
+                context.AUser1).Wait());
+        }
+
+        [Fact]
+        public void SendMessageToNonExtantChannelFails()
+        {
+            Assert.Throws<AggregateException>(() => context.messageDomain.SendMessage(new Message
+            {
+                MessageSubject = $"This message created at {DateTime.UtcNow}",
+                MessageText = $"{TestUtils.GetRandomString(6)} My dearest {context.AUser2.FirstName}, I hope this component test passes and finds you well. Happy {DateTime.UtcNow.DayOfWeek}! -{context.AUser1.FirstName}",
+                SenderUserName = context.AUser1.UserName,
+                ChannelId = -1
+            },
+                context.AUser1).Wait());
+        }
+
+        [Fact]
+        public void RetrieveMessagesWorks()
+        {
+            var receiver = context.RandomUser();
+            var sender = context.RandomUser();
+            context.userAccountDomain.CreateAccount(receiver).Wait();
+            context.userAccountDomain.CreateAccount(sender).Wait();
+
+            var channelId = context.messageDomain.CreateChannel(sender, $"{TestUtils.GetRandomString(6)} Component Test Friends", new List<string>() { receiver.UserName }).Result;
+            var messageIDs = context.SendMessage(sender, receiver, 30, channelId);
+
+            var conversationChannels = context.messageDomain.GetMessages(false, null, receiver).Result;
+
+            Assert.Single(conversationChannels);
+            Assert.Equal(messageIDs.Count, conversationChannels.SelectMany(cc => cc.Messages).Count());
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, false)]
+        [InlineData(false, true)]
+        public void MessagesStatusChangesMessagesRetrieved(bool delete, bool read)
+        {
+            var receiver = context.RandomUser();
+            var sender = context.RandomUser();
+            context.userAccountDomain.CreateAccount(receiver).Wait();
+            context.userAccountDomain.CreateAccount(sender).Wait();
+
+            var channelId = context.messageDomain.CreateChannel(sender, $"{TestUtils.GetRandomString(6)} Component Test Friends", new List<string>() { receiver.UserName }).Result;
+            var originalMessageIds = context.SendMessage(sender, receiver, 30, channelId);
+
+            var messagesAcrossAllChannels = context.messageDomain.GetMessages(false, null, receiver).Result;
+            Assert.Single(messagesAcrossAllChannels); //this test is single channel only
+            Assert.Equal(originalMessageIds.Count, messagesAcrossAllChannels.SelectMany(cc => cc.Messages).Count());
+
+            int reducedBy = 0;
+            List<int> disallowedMessageIds = new List<int>();
+            if (read)
+            {
+                reducedBy += 1;
+                context.messageDomain.DeleteMessage(new List<int> { originalMessageIds[12] }, receiver).Wait();
+                disallowedMessageIds.Add(originalMessageIds[12]);
+
+            }
+            if (delete)
+            {
+                reducedBy += 1; 
+                context.messageDomain.DeleteMessage(new List<int> { originalMessageIds[6] }, receiver).Wait();
+                disallowedMessageIds.Add(originalMessageIds[6]);
+            }
+
+            messagesAcrossAllChannels = context.messageDomain.GetMessages(true, null, receiver).Result;
+            Assert.Equal(originalMessageIds.Count - reducedBy, messagesAcrossAllChannels.SelectMany(cc => cc.Messages).Count());
+
+            var mergedConversationsMessageIds = messagesAcrossAllChannels.SelectMany(cc => cc.Messages).Select(m => m.MessageId);
+
+            foreach (int removedMessageId in disallowedMessageIds)
+            {
+                Assert.DoesNotContain(removedMessageId, mergedConversationsMessageIds);
+            }
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RetrievePagingGetsAllMessages(bool unreadOnly)
+        {
+            var receiver = context.RandomUser();
+            var sender = context.RandomUser();
+            context.userAccountDomain.CreateAccount(receiver).Wait();
+            context.userAccountDomain.CreateAccount(sender).Wait();
+            var channelId = context.messageDomain.CreateChannel(sender, "Component Test Friends", new List<string>() { receiver.UserName }).Result;
+            var messageIDs = context.SendMessage(sender, receiver, 300, channelId);
+
+
+            var messagesAcrossAllChannels = context.messageDomain.GetMessages(unreadOnly, null, receiver).Result;
+            var mergedConversations = messagesAcrossAllChannels.SelectMany(cc => cc.Messages);
+            Assert.Equal(100, mergedConversations.Count()); //we only page out 100 messages at once
+
+            messagesAcrossAllChannels = context.messageDomain.GetMessages(unreadOnly, mergedConversations.Last().MessageId, receiver).Result;
+            var mergedConversations2 = messagesAcrossAllChannels.SelectMany(cc => cc.Messages);
+            Assert.Equal(100, mergedConversations2.Count()); //we only page out 100 messages at once
+
+            Assert.False(mergedConversations2.Any(m => mergedConversations.Any(m2 => m2.MessageId == m.MessageId)));
+
+            messagesAcrossAllChannels = context.messageDomain.GetMessages(unreadOnly, mergedConversations2.Last().MessageId, receiver).Result;
+            var mergedConversations3 = messagesAcrossAllChannels.SelectMany(cc => cc.Messages);
+            Assert.Equal(100, mergedConversations3.Count()); //we only page out 100 messages at once
+
+            messagesAcrossAllChannels = context.messageDomain.GetMessages(unreadOnly, mergedConversations3.Last().MessageId, receiver).Result;
+            var messageBatchLast = messagesAcrossAllChannels.SelectMany(cc => cc.Messages);
+            Assert.Empty(messageBatchLast); //300 sent, 300 retrieved, none left
+
+
+        }
+    }
+}
