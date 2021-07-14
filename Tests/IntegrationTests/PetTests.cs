f8018cfd195d1de7d0502cf650a7e59194c0f90b
Jeremy Barnes <Jeremiah.Barnes@outlook.com>
2021-07-14 09:53:05 -0500

    XUnit threading weirdness, command timeouts

--- Tests/IntegrationTests/PetTests.cs
+++ Tests/IntegrationTests/PetTests.cs
@@ -42,7 +42,7 @@ namespace Tests.IntegrationTests
         {
             {
                 DBConn = GetNewDbConnection();
-                DBConn.Open();
+                //DBConn.Open();
                 PetColor1 = CfgRepo.CreatePetColor(new PetColorConfig() { Name = Guid.NewGuid().ToString().Substring(0, 5), ImagePatternPath = "8clFw0e.jpg" }).Result;
                 PetColor2 = CfgRepo.CreatePetColor(new PetColorConfig() { Name = Guid.NewGuid().ToString().Substring(0, 5), ImagePatternPath = "8clFw0e.jpg" }).Result;
                 PetSpecies1 = CfgRepo.CreatePetSpecies(new PetSpeciesConfig() { Name = Guid.NewGuid().ToString().Substring(0, 5), Description = "", MaxHitPoints = 1000, ImageBasePath = "https://i.imgur.com/" }).Result;
@@ -219,20 +219,69 @@ namespace Tests.IntegrationTests
         [Fact]
         public void CreateWithInvalidColorFails()
         {
-            var nonPersistedPet = Context.RandomPetNotPersisted(Int32.MinValue, Context.PetSpecies1, Context.OwnerUser1.UserId);
-            Assert.ThrowsAsync<CritterException>(() => PetDomain.CreatePet(nonPersistedPet, Context.OwnerUser1));
+            try
+            {
+                var nonPersistedPet = Context.RandomPetNotPersisted(Int32.MinValue, Context.PetSpecies1, Context.OwnerUser1.UserId);
+                Assert.ThrowsAsync<CritterException>(() => PetDomain.CreatePet(nonPersistedPet, Context.OwnerUser1));
+            }
+            catch (AggregateException a)
+            {
+                Console.WriteLine(a.Message);
+                foreach (Exception innerException in a.Flatten().InnerExceptions)
+                {
+                    Console.WriteLine(innerException.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
         }
 
         [Fact]
         public void RetrieveSpecies()
         {
-            Assert.NotEmpty(PetDomain.RetrieveAvailableSpecies(Context.OwnerUser1).Result);
+            try
+            {
+                Assert.NotEmpty(PetDomain.RetrieveAvailableSpecies(Context.OwnerUser1).Result);
+            }
+            catch (AggregateException a)
+            {
+                Console.WriteLine(a.Message);
+                foreach (Exception innerException in a.Flatten().InnerExceptions)
+                {
+                    Console.WriteLine(innerException.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
         }
 
         [Fact]
         public void RetrieveColors()
         {
-            Assert.NotEmpty(PetDomain.RetrieveAvailableColors(Context.OwnerUser1).Result);
+            try
+            {
+                Assert.NotEmpty(PetDomain.RetrieveAvailableColors(Context.OwnerUser1).Result);
+            }
+            catch (AggregateException a)
+            {
+                Console.WriteLine(a.Message);
+                foreach (Exception innerException in a.Flatten().InnerExceptions)
+                {
+                    Console.WriteLine(innerException.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
         }
 
     }
